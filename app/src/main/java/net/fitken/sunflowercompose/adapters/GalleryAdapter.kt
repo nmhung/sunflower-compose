e628a5c895973d8143b66e763f189ac6f87785bc
Hung Nguyen <>
2021-07-25 10:01:04 +0700

    add sunflower from original source

---
+++ app/src/main/java/net/fitken/sunflowercompose/adapters/GalleryAdapter.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fitken.sunflowercompose.adapters
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import net.fitken.sunflowercompose.GalleryFragment
+import net.fitken.sunflowercompose.adapters.GalleryAdapter.GalleryViewHolder
+import net.fitken.sunflowercompose.data.UnsplashPhoto
+import net.fitken.sunflowercompose.databinding.ListItemPhotoBinding
+
+/**
+ * Adapter for the [RecyclerView] in [GalleryFragment].
+ */
+
+class GalleryAdapter : PagingDataAdapter<UnsplashPhoto, GalleryViewHolder>(GalleryDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryViewHolder {
+        return GalleryViewHolder(
+            ListItemPhotoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: GalleryViewHolder, position: Int) {
+        val photo = getItem(position)
+        if (photo != null) {
+            holder.bind(photo)
+        }
+    }
+
+    class GalleryViewHolder(
+        private val binding: ListItemPhotoBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.setClickListener { view ->
+                binding.photo?.let { photo ->
+                    val uri = Uri.parse(photo.user.attributionUrl)
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    view.context.startActivity(intent)
+                }
+            }
+        }
+
+        fun bind(item: UnsplashPhoto) {
+            binding.apply {
+                photo = item
+                executePendingBindings()
+            }
+        }
+    }
+}
+
+private class GalleryDiffCallback : DiffUtil.ItemCallback<UnsplashPhoto>() {
+    override fun areItemsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto): Boolean {
+        return oldItem == newItem
+    }
+}
